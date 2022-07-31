2e68b0daebbe359f1ce5e1bb19d05240efefa467
Jacobo Geada Ansino <geada@hpe.com>
2022-07-31 14:19:49 +0200

    feat: modulos

---
+++ wallitaspringbacth/src/main/java/org/wallita/WallitaApplication.java
@@ -0,0 +1,13 @@
+package org.wallita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallitaApplication.class, args);
+	}
+
+}
---
+++ wallitaspringbacth/src/test/java/org/wallita/WallitaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wallita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
